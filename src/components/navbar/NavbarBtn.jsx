501aec23c5af5fd85f47736db87a8e7031709e6f
nightshiftmaster <nightshiftmaster@gmail.com>
2025-03-17 17:31:44 +0200

    update cvv button

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import { LuArrowDownRight } from "react-icons/lu";
 
 export const NavbarBtn = () => {
-  // return (
-  //   <button
-  //     type="button"
-  //     className="px-4 py-2 rounded-full text-xl text-white border-cyan border flex items-center gap-1 font-bold bg-gradient-to-r from-cyan to-orange hover:scale-110 hover:border-orange transition-all duration-500 hover:shadow-cyanShadow"
-  //   >
-  //     Hire Me
-  //     <div className="sm:hidden md:block">
-  //       <LuArrowDownRight />
-  //     </div>
-  //   </button>
-  // );
   return (
     <a
       href="/"
       download="VLAD_FRONTEND.pdf"
-      className="px-4 py-2 rounded-full text-xl text-white border-cyan border flex items-center gap-1 font-bold bg-gradient-to-r from-cyan to-orange hover:scale-110 hover:border-orange transition-all duration-500 hover:shadow-cyanShadow"
+      className="px-4 py-2 rounded-full text-xl  text-white border-cyan border flex items-center gap-1 font-bold bg-gradient-to-r from-cyan to-orange hover:scale-110 hover:border-orange transition-all duration-500 hover:shadow-cyanShadow"
     >
-      Download CV
+      <p className="text-sm md:text-xl text-center"> Download CV</p>
+
       <div className="sm:hidden md:block">
         <LuArrowDownRight />
       </div>
