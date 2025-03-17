bba24e2dc3e5efa5c513b9cba49fb1e0c061f067
nightshiftmaster <nightshiftmaster@gmail.com>
2025-03-17 18:27:40 +0200

    download pdf

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -4,8 +4,8 @@ import { LuArrowDownRight } from "react-icons/lu";
 export const NavbarBtn = () => {
   return (
     <a
-      href="/"
-      download="VLAD_FRONTEND.pdf"
+      href="/images/VLAD_FRONTEND.pdf"
+      download
       className="px-4 py-2 rounded-full text-xl  text-white border-cyan border flex items-center gap-1 font-bold bg-gradient-to-r from-cyan to-orange hover:scale-110 hover:border-orange transition-all duration-500 hover:shadow-cyanShadow"
     >
       <p className="text-sm md:text-xl text-center"> Download CV</p>
